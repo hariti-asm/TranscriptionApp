7a570665697dfe2d6c66d7da4b2ed7e691661e10
Asmae Hariti <haritiasmaa74@gmail.com>
2023-06-15 00:38:35 +0100

    dl pz

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+@props(['align' => 'right', 'width' => '48', 'NotePolicyClasses' => 'py-1 bg-white'])
 
 @php
 switch ($align) {
@@ -36,8 +36,8 @@ switch ($width) {
             class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
             style="display: none;"
             @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{-- {{ $content }} --}}
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $NotePolicyClasses }}">
+            {{-- {{ $NotePolicy }} --}}
         </div>
     </div>
 </div>
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -4,49 +4,43 @@
     </p>
 @endif
 
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <form method="POST" action="{{ route('notes.store') }}">
-            @csrf
-        <input name="upload" type="file" accept="audio/x-aiff, audio/flac, audio/mpeg, audio/ogg, audio/wav, audio/mp3" />
 
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
-                <button type="submit">upload</button>
-        </form>
 
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-            {{-- <span class="text-gray-800">{{ $note->user->name }}</span> --}}
+<form method="POST" action="{{ route('notes.store') }}" enctype="multipart/form-data">
+    <!-- Laravel XSRF token injection -->
+    @csrf
 
-            @foreach ($notes as $note)
-                <div class="p-2 flex space-x-2">
+    <!-- Form field where the file will be selected -->
+    <input name="upload" type="file" accept="audio/x-aiff, audio/flac, audio/mpeg, audio/ogg, audio/wav, audio/mp3" />
 
-                    <div class="flex-1">
+    <!-- Button to submit our form -->
+    <button type="submit">Upload</button>
+
+</form>
+    <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+        @foreach ($notes as $note)
+            <div class="p-6 flex space-x-2">
+                <div class="flex-1">
+                    <div class="flex justify-between items-center">
+                        <p class="mt-4 text-lg text-gray-900">{{ $note->upload }}</p>
                         <div class="flex justify-between items-center">
-                            <div>
-                                {{-- <small class="ml-2 text-sm text-gray-600">{{ $note->created_at->format('j M Y, g:i a') }}</small> --}}
-                                <p class="ml-4 text-xs text-gray-900">{{ $note->upload }}</p>
-                                <audio controls>
-                                    {{-- <source src="{{ asset('../uploads/' . $note->upload) }}" > --}}
-                                </audio>
-                            </div>
-
-
-                                <div>
-
-                                        {{-- <form method="POST"  class="flex" action="{{ route('notes.destroy', $note) }}" >
-                                            @csrf
-                                            <x-dropdown-link :href="route('notes.destroy', $note)" onclick="event.preventDefault(); this.closest('form').submit();" class="bg-blue-600">
-                                                {{ __('Transcribe') }}
-                                            </x-dropdown-link>
-                                            @method('delete')
-                                            <x-dropdown-link :href="route('notes.destroy', $note)" onclick="event.preventDefault(); this.closest('form').submit();">
-                                                {{ __('Delete') }}
-                                            </x-dropdown-link>
-
-                                        </form> --}}
-                                </div>
+                            <audio controls class="flex-1">
+                                {{-- <source src="{{ asset('../uploads/' . $note->upload) }}"> --}}
+                            </audio>
+                            @if ($note->user->is(auth()->user()))
+                                <form method="POST" action="{{ route('notes.destroy', $note) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" onclick="event.preventDefault(); this.closest('form').submit();" class="your-button-styles">
+                                        {{ __('Delete') }}
+                                    </button>
+                                </form>
+                            @endif
                         </div>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
+
+    <div class="upload-wrapper body-small" role="presentation" tabindex="0"><input accept="audio/mp4,.mp4,.m4a,audio/mp3,.mp3,video/webm,.webm" type="file" tabindex="-1" style="display: none;"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="32px" width="32px" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg><h4 class="upload-title">Upload audio from computer</h4><p class="upload-subtitle">or drag and drop here</p><p class="upload-subtitle">(.mp4, .m4a, .mp3, .webm)</p></div>
